f550a3d0966d99aad1ca1a0821570249ec007033
elizalap <elizapaoliello@outlook.com>
2022-12-12 20:51:00 -0300

    Implementando relacionamento N para N

--- laravel+vue.js/code/app_super_gestao/app/Item.php
+++ laravel+vue.js/code/app_super_gestao/app/Item.php
@@ -4,7 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * Model Item que mapeia a tabela produtos tem um (hasOne) ItemDetalhe
+ * que mapeia a tabela produto_detalhes
+ */
 class Item extends Model
 {
-    //
+    protected $table = 'produtos';
+    protected $fillable = ['nome', 'descricao', 'peso', 'unidade_id', 'fornecedor_id'];
+
+    public function itemDetalhe()
+    {
+        return $this->hasOne('App\ItemDetalhe', 'produto_id', 'id');
+    }
+
+    public function fornecedor()
+    {
+        return $this->belongsTo('App\Fornecedor');
+    }
 }
--- laravel+vue.js/code/app_super_gestao/resources/views/app/produto/_components/form_create_edit.blade.php
+++ laravel+vue.js/code/app_super_gestao/resources/views/app/produto/_components/form_create_edit.blade.php
@@ -6,6 +6,13 @@
       <form method="post" action="{{ route('produto.store') }}">
          @csrf
 @endif
+       <select name="fornecedor_id">
+            <option value="">-- Selecione um fornecedor --</option>
+            @foreach($fornecedores as $fornecedor)
+            <option value="{{$fornecedor->id}}" {{($produto->fornecedor_id ?? old('fornecedor_id')) == $fornecedor->id ? 'selected' : ''}}>{{$fornecedor->nome}}</option>
+            @endforeach                      
+       </select>
+
        <input type="text" name="nome" value="{{$produto->nome ?? old('nome')}}" placeholder="Nome" class="borda-preta">
          {{ $errors->has('nome') ? $errors->first('nome') : '' }}
          
