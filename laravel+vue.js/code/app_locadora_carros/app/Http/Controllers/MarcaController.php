dc500b3372b43b4ccd72423392f8248b28766ba8
elizalap <elizapaoliello@outlook.com>
2022-12-29 10:19:44 -0300

    Implementando Repository Pattern

--- laravel+vue.js/code/app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ laravel+vue.js/code/app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -5,24 +5,39 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Storage;
 use App\Models\Marca;
 use Illuminate\Http\Request;
+use App\Repositories\MarcaRepository;
 
 class MarcaController extends Controller
 {
-    public function __construct(Marca $marca)
-    {
+    public function __construct(Marca $marca) {
         $this->marca = $marca;
     }
-
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //$marcas = Marca::all();
-        $marcas = $this->marca->all();
-        return response()->json($marcas, 200);
+
+        $marcaRepository = new MarcaRepository($this->marca);
+
+        if($request->has('atributos_modelos')) {
+            $atributos_modelos = 'modelos:id,'.$request->atributos_modelos;
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
+        } else {
+            $marcaRepository->selectAtributosRegistrosRelacionados('modelos');
+        }
+
+        if($request->has('filtro')) {
+            $marcaRepository->filtro($request->filtro);
+        }
+
+        if($request->has('atributos')) {
+            $marcaRepository->selectAtributos($request->atributos);
+        } 
+
+        return response()->json($marcaRepository->getResultado(), 200);
     }
 
     /**
@@ -43,9 +58,6 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        //$marca = Marca::create($request->all());
-
-        //Encaminhar o 'accept' no header da requisição!
         $request->validate($this->marca->rules(), $this->marca->feedback());
 
         $imagem = $request->file('imagem');
@@ -67,11 +79,12 @@ class MarcaController extends Controller
      */
     public function show($id)
     {
-        $marca = $this->marca->find($id);
-        if ($marca === null) {
-            return response()->json(['erro' => 'Recurso pesquisado não existe.'], 404);
-        }
-        return response()->json($marca, 201);
+        $marca = $this->marca->with('modelos')->find($id);
+        if($marca === null) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404) ;
+        } 
+
+        return response()->json($marca, 200);
     }
 
     /**
@@ -94,43 +107,50 @@ class MarcaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //$marca->update($request->all());
         $marca = $this->marca->find($id);
 
-        if ($marca === null) {
-            return response()->json(['erro' => 'Impossível realizar atualização. O recurso solicitado não existe.'], 404);
+        if($marca === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
         }
 
-        if ($request->method() === 'PATCH') {
+        if($request->method() === 'PATCH') {
+
             $regrasDinamicas = array();
-            $teste = '';
 
             //percorrendo todas as regras definidas no Model
-            foreach ($marca->rules() as $input => $regra) {
-
-                //coletar apenas as regras aplicaveis aos parametros parciais da requisição PATCH
-                if (array_key_exists($input, $request->all())) {
+            foreach($marca->rules() as $input => $regra) {
+                
+                //coletar apenas as regras aplicáveis aos parâmetros parciais da requisição PATCH
+                if(array_key_exists($input, $request->all())) {
                     $regrasDinamicas[$input] = $regra;
                 }
             }
-
+            
             $request->validate($regrasDinamicas, $marca->feedback());
+
         } else {
             $request->validate($marca->rules(), $marca->feedback());
         }
-
+        
         //remove o arquivo antigo caso um novo arquivo tenha sido enviado no request
-        if ($request->file('imagem')) {
+        if($request->file('imagem')) {
             Storage::disk('public')->delete($marca->imagem);
         }
-
+        
         $imagem = $request->file('imagem');
         $imagem_urn = $imagem->store('imagens', 'public');
 
+        //preencher o objeto $marca com os dados do request
+        $marca->fill($request->all());
+        $marca->imagem = $imagem_urn;
+        //dd($marca->getAttributes());
+        $marca->save();
+        /*
         $marca->update([
             'nome' => $request->nome,
             'imagem' => $imagem_urn
         ]);
+        */
 
         return response()->json($marca, 200);
     }
@@ -145,14 +165,15 @@ class MarcaController extends Controller
     {
         $marca = $this->marca->find($id);
 
-        if ($marca === null) {
-            return response()->json(['Impossível realizar a exclusão. O recursos solicitado não existe.'], 404);
+        if($marca === null) {
+            return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe'], 404);
         }
 
-        //remove o arquivo antigo 
-        Storage::disk('public')->delete($marca->imagem);
+        //remove o arquivo antigo
+        Storage::disk('public')->delete($marca->imagem);        
 
         $marca->delete();
-        return response()->json(['msg' => 'A marca foi removida com sucesso.'], 200);
+        return response()->json(['msg' => 'A marca foi removida com sucesso!'], 200);
+        
     }
 }
