af8d282d05fff69dab9403603549d5e8cb03b16c
elizalap <elizapaoliello@outlook.com>
2023-01-05 22:19:23 -0300

    Criando página de tarefas com laravel puro

---
+++ DIO - livecoding Usando o livewarepara dar mais vida a aplicações laravel/meu-projeto-liveware/resources/views/tarefas/index.blade.php
@@ -0,0 +1,32 @@
+<h1>Tarefas</h1>
+
+<form action="{{route('tarefas.index')}}" method="GET">
+   @csrf
+   <input type="text" name="busca" placeholder="Buscar Tarefa">
+</form>
+<table>
+   <thead>
+      <tr>
+         <th>Id</th>
+         <th>Categoria</th>
+         <th>Nome</th>
+         <th>Conclusão</th>
+         <th></th>
+      </tr>
+   </thead>
+   <tbody>
+      @foreach ($tarefas as $tarefa)
+         <tr>
+            <td>{{$tarefa->id}}</td>
+            <td>{{$tarefa->categoria}}</td>
+            <td>{{$tarefa->titulo}}</td>
+            <td>{{$tarefa->concluida}}</td>
+            <td>
+               <a href="#">Editar</a>
+               <a href="#">Excluir</a>
+            </td>
+         </tr>
+
+      @endforeach
+   </tbody>
+</table>
